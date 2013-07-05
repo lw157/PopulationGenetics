9d27dc71e9abf0d7468fb8c3d5c973f41d01add8
lw157 <wallacewly@gmail.com>
2013-07-05 15:21:59 -0400

    Update Pedtrios.py

--- Pedtrios.py
+++ Pedtrios.py
@@ -9,12 +9,9 @@ def trio(infile,outfile):
     f = open(infile,"r")
     out = open(outfile,"w")
 
-    print "Inputfile is " + infile
-    print "Output will be " + outfile
-
     trios = set()
     count_trios = 0
-    count_sigleparents = 0
+    count_singleparents = 0
 
     for i in map(string.strip,f):
         ls = i.split()
@@ -31,35 +28,34 @@ def trio(infile,outfile):
             count_singleparents += 1
 
         if (not re.match(r'NA',ls[3],re.M|re.I)) and re.match(r'NA',ls[4], re.M|re.I):
-            conut_singleparents += 1
-            
-        with open(infile,"r") as ff:
-            for j in map(string.strip,ff):
-                l = j.split()
-                if l[1] in trios:
-                    out.write(j + "\n")
+            count_singleparents += 1
+
+    with open(infile,"r") as ff:
+        for j in map(string.strip,ff):
+            l = j.split()
+            if l[1] in trios:
+                out.write(j + "\n")
 
     f.close()
     out.close()
 
-    print "There are " + count_trios + " trios in " + infile
-    print "There are " + count_singleparents + " single parents in " + infile
-
-
+    print "There are " + str(count_trios) + " trios in "
+    print "There are " + str(count_singleparents) + " single parents in "
+    
+    
 def usage():
     print 'This python script will count number of trios, parents\n'
     print 'Usage: python Pedtrios.py -i <inputfile> -o <outputfile> \n'
     print 'Method: >>> count number of family member from plink Ped file'
     print '        >>> out put unique family in Plink Ped format \n'
-    print 'Options: python cntped.py input.ped'
-    sys.exit(' ')
+    sys.exit('')
 
 def main(argv):
     inputfile = ''
     outputfile = ''
 
     try:
-        opts, args = getopt.getopt(argv,"hi:o", ["ifile=","ofile="])
+        opts, args = getopt.getopt(argv,"h:i:o:", ["help","ifile=","ofile="])
     except getopt.GetoptError:
         usage()
         sys.exit(2)
@@ -70,16 +66,15 @@ def main(argv):
             sys.exit()
         elif opt in ("-i", "--ifile"):
             inputfile = arg
-        elif opt in ("-o","--ofile"):
+        elif opt in ("-o", "--ofile"):
             outputfile = arg
         else:
             usage()
             sys.exit(2)
 
-        if inputfile and outputfile:
-            trio(inputfile,outputfile)
+    if inputfile and outputfile:
+        trio(inputfile,outputfile)
 
-        print "analysis is done\n"
 
 if __name__ == "__main__":
     if len(sys.argv) <= 1:
@@ -87,3 +82,5 @@ if __name__ == "__main__":
         sys.exit()
     else:
         main(sys.argv[1:])
+
+        print "analysis is done\n"
