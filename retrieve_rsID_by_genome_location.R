d254cef9ace0c41dcfe281bed01aacd343b8eb14
Liuyang Wang <wallacewly@gmail.com>
2020-06-04 10:03:40 -0400

    Create retrieve_rsID_by_genome_location.R
    To get SNPs' rsID based on genome coordinates. It is not ideal for > 200 SNPs, because it is slow.

---
+++ retrieve_rsID_by_genome_location.R
@@ -0,0 +1,22 @@
+## modify from stackoverflow
+
+library(biomaRt)
+listEnsemblArchives() 
+
+coords = data.frame(chr_name = c(1,1), start = c('12554805','41036621'), end=c('12554805','41036621'))
+coords$ID = paste0("id", 1:nrow(coords)) ## This unique ID for merging data
+
+# snps38 =useMart(biomart="ENSEMBL_MART_SNP", dataset="hsapiens_gene_ensembl") # GRCh38
+snps = useMart(biomart="ENSEMBL_MART_SNP", host="grch37.ensembl.org", path="/biomart/martservice",dataset="hsapiens_snp") ## hg19
+
+res <- list()
+
+for ( i in 1:nrow(ncoord)){
+  ens=getBM(attributes = c('refsnp_id','chr_name','chrom_start', 'chrom_end', 'allele_1'), filters = c('chr_name','start','end'),
+            values = as.list(coords[i,]), mart = snps)
+  res[[i]] = ens
+}
+
+res_out = plyr::ldply(res, .id = "ID")
+
+res_fin = merge(res_out, coords, by = "id")
