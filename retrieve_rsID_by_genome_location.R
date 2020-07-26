f6ff83d67de9ebff19c11f5cd87b519eefaa03b5
Liuyang Wang <wallacewly@gmail.com>
2020-07-26 17:21:17 -0400

    Update retrieve_rsID_by_genome_location.R

--- retrieve_rsID_by_genome_location.R
+++ retrieve_rsID_by_genome_location.R
@@ -9,7 +9,7 @@ listEnsembl(version = 100)
 coords = data.frame(chr_name = c(1,1), start = c(12554805,41036621), end=c(12554805,41036621))
 coords$ID = paste0("id", 1:nrow(coords)) ## This unique ID for merging data
 
-snpmart = useEnsembl(biomart = "snp", dataset="hsapiens_snp") ## This is for biomaRt 2.44.1
+snpmart = useEnsembl(biomart = "snp", dataset="hsapiens_snp") ## This is for biomaRt 2.44.1 + GRCh38
 # snps38 =useMart(biomart="ENSEMBL_MART_SNP", dataset="hsapiens_gene_ensembl") # GRCh38
 # snps = useMart(biomart="ENSEMBL_MART_SNP", host="grch37.ensembl.org", path="/biomart/martservice",dataset="hsapiens_snp") ## hg19
 
