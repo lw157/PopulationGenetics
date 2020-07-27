c0f7faddec8202a73421c20980d923a7df495cde
Liuyang Wang <wallacewly@gmail.com>
2020-07-26 21:06:44 -0400

    Update batch_ucsc_getSNP_bybed.sh

--- batch_ucsc_getSNP_bybed.sh
+++ batch_ucsc_getSNP_bybed.sh
@@ -3,4 +3,13 @@
 #conda install -c bioconda perl-dbi
 #conda install -c bioconda perl-dbd-mysql
 
+## single region
 echo "chr1 1 1000000" | perl batchUCSC.pl -p snp
+
+## batch 
+perl batchUCSC.pl -p snp151::: -d hg38 input.bed > out.annot.txt
+
+## cat input.bed
+#1	2103939	2103940
+#1	2104244	2104245
+#1	2104359	2104360
