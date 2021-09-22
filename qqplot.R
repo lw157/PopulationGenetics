c6c33a57ecdf899fa3060fd723ef6a8ca3c6ca15
Liuyang Wang <wallacewly@gmail.com>
2021-09-22 10:48:44 -0400

    Update qqplot.R

--- qqplot.R
+++ qqplot.R
@@ -1,14 +1,28 @@
 ## plot qq with CI using ggplot2 styple
-## adapted from Kamil Slowikowski's gg_qqplot
+## adapted from Kamil Slowikowski's gg_qqplot, but with the "fast" features to thin the SNPs. This is very useful when there are millions of SNPs
 
-qqplot <- function(pval, ci = 0.95) {
+qqplot <- function(pval, fast = FALSE, ci = 0.95) {
   N <- length(pval)
-  dat <- data.frame(obs = -log10(sort(pval)),
-                   exp = -log10(ppoints(N)),
-                   clow  = -log10(qbeta(p = (1 - ci) / 2, shape1 = 1:N, shape2 = N:1)),
-                   cupp   = -log10(qbeta(p = (1 + ci) / 2, shape1 = 1:N, shape2 = N:1))  )
+  xpvs = sort(pval)
+  ypvs = sort(ppoints(N))
+  
+  keep = seq_along(ypvs)
+  if (fast == TRUE){ 
+    if (length(ypvs) > 1000) {
+        xlv = as.integer((xpvs - xpvs[1])/diff(range(xpvs)) * 2000)
+        ylv = as.integer((ypvs - ypvs[1])/diff(range(ypvs)) * 2000)
+        keep = c(TRUE, (diff(xlv) != 0) | (diff(ylv) != 0) )
+      } 
+  }
+  
+  dat <- data.frame(obs = -log10(xpvs[keep]),
+                   exp = -log10(ypvs[keep]),
+                   clow  = -log10(qbeta(p = (1 - ci) / 2, shape1 = 1:N, shape2 = N:1))[keep],
+                   cupp   = -log10(qbeta(p = (1 + ci) / 2, shape1 = 1:N, shape2 = N:1))[keep]
+                   )
   xlabels <- expression(paste("Expected ( -log"[10], plain(P), " )"))
   ylabels <- expression(paste("Observed (-log"[10], plain(P), " )" ))
+  
   p = ggplot(dat) +
     geom_point(aes(exp, obs), shape = 20, size = 3) +
     geom_abline(intercept = 0, slope = 1,color = "red", linetype="dashed") +
@@ -34,5 +48,5 @@ inflation_adjust <- function(PVALUE = NULL){
 '# 
 '# p = data.table::fread("gwas.txt")
 '# p_adj = inflation_adjust(PVALUE = p$PVALUE)
-'# qqplot(pval = p_adj$p_raw, ci = 0.95) ## qqplot for raw p values
-'# qqplot(pval = p_adj$p_adj, ci = 0.95) # qq plot for adjusted p value
+'# qqplot(pval = p_adj$p_raw, fast=T, ci = 0.95) ## qqplot for raw p values
+'# qqplot(pval = p_adj$p_adj, fast = T, ci = 0.95) # qq plot for adjusted p value
